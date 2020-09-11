d7051a5d23d4d0838b5fe59f9e2d2c93d893c198
anil.saravade@searce.com <anil.saravade@searce.com>
2020-09-11 13:34:34 +0530

    uploading files

---
+++ setup/docker/hook.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+__author__ = "Anil Saravade"
+__copyright__ = "Copyright (©) 2020. SwapStyle. All rights reserved."
+
+## requirements.txt
+# Flask==0.10.1
+# itsdangerous==0.24
+# Jinja2==2.8
+# MarkupSafe==0.23
+# Werkzeug==0.11.9
+# requests
+
+import os
+
+from datetime import datetime, timedelta
+from flask import Flask, request, abort, jsonify
+import requests
+import json
+import hmac
+import hashlib
+
+WEBHOOK_VERIFY_TOKEN = os.getenv('WEBHOOK_VERIFY_TOKEN')
+CIRCLE_API_USER_TOKEN = os.getenv('CIRCLE_API_USER_TOKEN')
+CIRCLE_URL = os.getenv('CIRCLE_URL')
+CIRCLE_DEPLOY_JOB = os.getenv('CIRCLE_DEPLOY_JOB')
+CIRCLE_REMOVE_JOB = os.getenv('CIRCLE_REMOVE_JOB')
+
+def temp_token():
+    import binascii
+    temp_token = binascii.hexlify(os.urandom(24))
+    return temp_token.decode('utf-8')
+
+def pr_action(request_json):
+    pr_action_list = ['opened', 'reopened', 'synchronize']
+    pr_action = request_json['action']
+    pr_number = request_json['number']
+    pr_branch = request_json['pull_request']['head']['ref']
+    if pr_action in pr_action_list:
+        if deploy_review(pr_number, pr_branch):
+            return True
+    elif pr_action == 'closed':
+        if remove_review(pr_number, pr_branch):
+            return True
+
+def deploy_review(pr_number, pr_branch):
+    headers = {
+        'Content-Type': 'application/json',
+        'Circle-Token': '{}'.format(CIRCLE_API_USER_TOKEN)
+    }
+    payload = {
+        "build_parameters": {
+            "CIRCLE_JOB": CIRCLE_DEPLOY_JOB,
+            "PR_NUMBER": pr_number,
+            "PR_BRANCH": pr_branch
+        }
+    }
+    url = '{}/{}'.format(CIRCLE_URL,pr_branch)
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code in [200, 201]:
+        return True
+
+
+def remove_review(pr_number, pr_branch):
+    headers = {
+        'Content-Type': 'application/json',
+        'Circle-Token': '{}'.format(CIRCLE_API_USER_TOKEN)
+    }
+    payload = {
+        "build_parameters": {
+            "CIRCLE_JOB": CIRCLE_REMOVE_JOB,
+            "PR_NUMBER": pr_number,
+            "PR_BRANCH": pr_branch
+        }
+    }
+    url = '{}/{}'.format(CIRCLE_URL,pr_branch)
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code in [200, 201]:
+        return True
+
+app = Flask(__name__)
+
+authorised_clients = {}
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    request_json = request.json
+    if request.method == 'GET':
+        return jsonify({'status':'success'}), 200
+
+    elif request.method == 'POST':
+        key = bytes(WEBHOOK_VERIFY_TOKEN, 'utf-8')
+        expected_signature = hmac.new(key=key, msg=request.data, digestmod=hashlib.sha1).hexdigest()
+        incoming_signature = request.headers.get('X-Hub-Signature').split('sha1=')[-1].strip()
+        if not hmac.compare_digest(incoming_signature, expected_signature):
+            return jsonify({'status':'not authorised'}), 401
+        else:
+            if pr_action(request_json):
+                response_json = {'status': 'success'}
+                return jsonify(response_json), 200,
+            else:
+                response_json = {'status': 'failed'}
+                return jsonify(response_json), 500, 
+    else:
+        abort(400)
+
+if __name__ == '__main__':
+    if WEBHOOK_VERIFY_TOKEN is None:
+        print('WEBHOOK_VERIFY_TOKEN has not been set in the environment.\nGenerating random token...')
+        token = temp_token()
+        print('Token: %s' % token)
+        WEBHOOK_VERIFY_TOKEN = token
+    app.run(host='0.0.0.0', debug=True)
